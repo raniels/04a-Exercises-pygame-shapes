0ad615871deb3b7a3898f56d56657d590af66a52
raniels <35283647+raniels@users.noreply.github.com>
2018-02-19 22:48:01 -0500

    finished excercise 4, made circles teal instead of white

--- exercise4.py
+++ exercise4.py
@@ -11,30 +11,32 @@ assert sys.version_info >= (3,4), 'This script requires at least Python 3.4'
 screen_size = (800,600)
 FPS = 60
 black = (0,0,0)
-white = (255,255,255)
+teal = (0,255,255)
 
 def main():
-	pygame.init()
-	screen = pygame.display.set_mode(screen_size)
-	font = pygame.font.SysFont("arial",64)
-	clock = pygame.time.Clock()
+    pygame.init()
+    screen = pygame.display.set_mode(screen_size)
+    font = pygame.font.SysFont("arial",64)
+    clock = pygame.time.Clock()
 
-	(x,y,radius) = (100,100,20)
-	
-	while True:
-		clock.tick(FPS)
+    (x,y,radius) = (100,100,30)
 
-		screen.fill(black)
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				pygame.quit()
-				sys.exit(0)
-			if event.type == pygame.MOUSEBUTTONUP:
-				pos = pygame.mouse.get_pos()
-		
-		pygame.draw.circle(screen, white, (x,y), radius)
-		
-		pygame.display.flip()
+    while True:
+        clock.tick(FPS)
+
+        screen.fill(black)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit(0)
+            if event.type == pygame.MOUSEBUTTONUP:
+                pos = pygame.mouse.get_pos()
+
+                print(str(pos))
+
+                pygame.draw.circle(screen, teal, pos, radius)
+
+                pygame.display.flip()
 
 if __name__ == '__main__':
-	main()
+    main()
